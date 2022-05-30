b25ff12212ed5562981e03e9a97b73512bb5a09a
dai <darope@163.com>
2022-05-30 21:22:38 +0800

    dev

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,28 @@ import React from "react";
 import logo from "./logo.svg";
 import sl from "./App.module.css";
 
+const menuList = ["Language", "我的订单", "客户服务", "合作中心"];
+
 function App() {
   return (
     <div>
       <div className={sl.topNavWrapper}>
-        <div className={sl.topNav}></div>
+        <div className={sl.topNav}>
+          <div className={sl.topLogin}>
+            <span>您好，请</span>
+            <a href="">登录</a>
+            <a href="">注册</a>
+            <img src="" alt="" />
+          </div>
+
+          <div className={sl.topNavRight}>
+            <ul>
+              {menuList.map((d) => (
+                <li>{d}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
